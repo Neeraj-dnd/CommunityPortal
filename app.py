46ca2a2f07ead37868d039f2558681b0690e218f
Neeraj-dnd <nksstudent00@gmail.com>
2023-10-06 10:36:07 +0530

    Updates - Sessions
    includes login sessions

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from flask_login import LoginManager, login_user, UserMixin
+# import smtplib
+# import socket
 
+
+# Configure app
 app = Flask(__name__)
+
+# Configure Session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 app.secret_key = 'noSecret'
 
@@ -18,8 +28,10 @@ mysql = MySQL(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 class User(UserMixin):
     pass
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -36,19 +48,24 @@ def load_user(user_id):
         return user
     return None
 
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    print("in '/':", session.get('username'))
+    if not session.get("username"):
+        return redirect("/login")
+    return render_template('logged.html')
 
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    msg = ''
-    if request.method == 'POST':
+    if request.method == 'GET' and not session.get('username'):
         username = request.form.get('username')
+        session['username'] = request.form.get("username")
+        print('session:', session['username'])
         password = request.form.get('password')
         print(f'username: {username}, password: {password}')
-        if username == '' or password == '':
+        if not username or not password:
             msg = 'Please fill both!'
             return render_template('signup.html', msg=msg)
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -60,17 +77,39 @@ def signup():
                 mysql.connection.commit()
                 cursor.close()
                 print('Signup successful!')
-                return render_template('logged.html')
+
+                # try:
+                #     message = 'You are Registered!'
+                #     server = smtplib.SMTP("smtp.gmail.com", 587)
+                #     server.starttls()
+                #     server.login("alahamora84@gmail.com", '')
+                #     server.sendmail("alhamora84@gmail.com", username, message)
+                #     print('Mail Sent !')
+                #     server.quit()
+                # except socket.gaierror as e:
+                #     print('Offline Error: %s', e)
+                # except ConnectionRefusedError as e:
+                #     print('Online Error: %s', e)
+                # except smtplib.SMTPNotSupportedError as e:
+                #     print('SMTP Not Supported Error: %s', e)
+                # except smtplib.SMTPAuthenticationError as e:
+                #     print('SMTP Authentication Error: %s', e)
+
+                return redirect('/logged')
         except MySQLdb.IntegrityError as e:
             mysql.connection.rollback()
             msg = 'Username already exists! Try Login'
             print(msg, e)
-    return render_template('signup.html', msg=msg)
+            return render_template('signup.html', msg=msg)
+    return redirect('/logged')
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST':
+    if request.method == 'POST' and not session.get('username'):
+        session['username'] = request.form.get("username")
+        print('session:', session['username'])
         username = request.form.get('username')
         password = request.form.get('password')
         print(f'username: {username} | password: {password}')
@@ -86,13 +125,23 @@ def login():
             user.username = user_data['userName']
             login_user(user)
             print("logged")
-            return render_template('logged.html')
+            return redirect('/logged')
         else:
             msg = 'Invalid credentials. Please try again.'
+
     return render_template('index.html', msg=msg)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session['username'] = None
+    print("you are logged out!")
+    return redirect('/')
+
 
 @app.route('/logged', methods=['GET', 'POST'])
 def logged():
+    print('logged in with:', session.get('username'))
     return render_template('logged.html')
 
 
